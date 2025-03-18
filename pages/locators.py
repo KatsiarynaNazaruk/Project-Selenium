7b773c35cea585160d14ac4f24fee868cab84223
Katsiaryna Nazaruk <eznak06@mail.ru>
2025-03-18 12:18:22 +0300

    folder_pages

---
+++ pages/locators.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    ITEM_NAME = (By.CSS_SELECTOR, '.product_main>h1')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alertinner>strong:nth-child(1)')
+    PRICE = (By.CSS_SELECTOR, '.product_main>.price_color')
+    PRICE_MESSAGE = (By.CSS_SELECTOR, '.alertinner>p>strong')
+    BASKET_BUTTON = (By.CSS_SELECTOR, '.basket-mini>span>a')
+    EMPTY_BASKET = (By.ID, "content_inner")
+    BASKET_EMPTY_MESSAGE = (By.XPATH, '//div[@id="content_inner"]//p[contains(text(),"Your basket is empty.")]')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CLASS_NAME, "icon-user")
